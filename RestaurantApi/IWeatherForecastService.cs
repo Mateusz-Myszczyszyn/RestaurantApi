06640cdde1f9ff6edc664eb449c0c7f37436b9cb
mateu <mateu@LAPTOP-SQF3B25B>
2022-06-03 15:23:39 +0200

    Add project files.

---
+++ RestaurantApi/IWeatherForecastService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantApi
+{
+    public interface IWeatherForecastService
+    {
+        IEnumerable<WeatherForecast> Get(int count, int minTemp, int maxTemp);
+    }
+}
