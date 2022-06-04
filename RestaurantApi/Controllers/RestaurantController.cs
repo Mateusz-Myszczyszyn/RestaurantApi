8486230df012276163e8a18a90688fdeea957d84
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-06-04 15:20:26 +0200

    Dtos Models

--- RestaurantApi/Controllers/RestaurantController.cs
+++ RestaurantApi/Controllers/RestaurantController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RestaurantApi.Entities;
+using RestaurantApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,18 +15,50 @@ namespace RestaurantApi.Controllers
     public class RestaurantController : ControllerBase
     {
         private readonly RestaurantDbContext _context;
+        private readonly IMapper _mapper;
 
-        public RestaurantController(RestaurantDbContext context)
+        public RestaurantController(RestaurantDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
-        public ActionResult<IEnumerable<Restaurant>> GetAll()
+        public ActionResult<IEnumerable<RestaurantDto>> GetAll()
         {
             var restaurants = _context
                 .Restaurants
+                .Include(r=>r.Address)
+                .Include(r => r.Dishes)
                 .ToList();
 
-            return Ok(restaurants);
+            var restaurantsDtos = _mapper.Map<List<RestaurantDto>>(restaurants);  
+            return Ok(restaurantsDtos);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<RestaurantDto> Get([FromRoute] int id)
+        {
+            var restaurant = _context
+                .Restaurants
+                .Include(r => r.Address)
+                .Include(r => r.Dishes)
+                .FirstOrDefault(r => r.Id == id);
+
+            if(restaurant is null)
+            {
+                return NotFound("Restaurant does not exist");
+            }
+
+            var restaurantDto = _mapper.Map<RestaurantDto>(restaurant);
+
+            return Ok(restaurantDto);
+        }
+
+        [HttpPost]
+        public ActionResult CreateRestaurant([FromBody]CreateRestaurantDto dto)
+        {
+            var restaurant = _mapper.Map<Restaurant>(dto);
+            _context.Restaurants.Add(restaurant);
+            _context.SaveChanges();
+            return Created($"/api/restaurant/{restaurant.Id}",null);
         }
     }
 }
---
+++ RestaurantApi/Models/DishDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApi.Models
+{
+    public class DishDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Description { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
