3108085a15d1a723d639a81e6b4156f096b3deda
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-06-27 14:59:36 +0200

    Database structure correction

--- RestaurantApi/Entities/Dish.cs
+++ RestaurantApi/Entities/Dish.cs
@@ -10,7 +10,7 @@ namespace RestaurantApi.Entities
     {
         public int Id { get; set; } 
         public string Name { get; set; }
-        public int Description { get; set; }
+        public string Description { get; set; }
 
         public decimal Price { get; set; }
 
