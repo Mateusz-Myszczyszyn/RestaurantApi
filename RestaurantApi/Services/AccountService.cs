ea6e15706e301560bb5d6cb5f86325ad7676f017
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-07-04 21:03:11 +0200

    Creating Service and UserDto to make AccountController more visible

---
+++ RestaurantApi/Services/AccountService.cs
@@ -0,0 +1,34 @@
+﻿using RestaurantApi.Entities;
+using RestaurantApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApi.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly RestaurantDbContext _context;
+
+        public AccountService(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+        public void RegisterUser(RegisterUserDto dto)
+        {
+            var newUser = new User()
+            {
+                Email = dto.Email,
+                DateOfBirth = dto.DateOfBirth,
+                Nationality = dto.Nationality,
+                RoleId = dto.RoleId
+            };
+
+            _context.Users.Add(newUser);
+            _context.SaveChanges();
+
+        }
+    }
+}
