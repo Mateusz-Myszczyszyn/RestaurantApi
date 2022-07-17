6644e6de66afab222d49bdbd0b9c437baa50c45e
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-07-17 17:25:45 +0200

    Adding another authorization handler to check if user has created more or equal than 2 restaurants.

---
+++ RestaurantApi/Authorization/CreatedMultipleRestaurantsRequirement.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApi.Authorization
+{
+    public class CreatedMultipleRestaurantsRequirement : IAuthorizationRequirement
+    {
+        public CreatedMultipleRestaurantsRequirement(int minimumRestaurantsCreated)
+        {
+            MinimumRestaurantsCreated = minimumRestaurantsCreated;
+        }
+
+        public int MinimumRestaurantsCreated { get; set; }
+    }
+}
