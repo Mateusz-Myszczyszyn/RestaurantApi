65db841c926b0a09a3db090f1cb1cd363eaf4712
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-07-17 16:41:40 +0200

    Adding migration to add  property CreatedById to Restaurant Entity,

--- RestaurantApi/Migrations/20220717125156_Authorization-restaurantCreatedbyidadd.Designer.cs
+++ RestaurantApi/Migrations/20220717125156_Authorization-restaurantCreatedbyidadd.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RestaurantApi.Entities;
 
 namespace RestaurantApi.Migrations
 {
     [DbContext(typeof(RestaurantDbContext))]
-    partial class RestaurantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220717125156_Authorization-restaurantCreatedbyidadd")]
+    partial class AuthorizationrestaurantCreatedbyidadd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
