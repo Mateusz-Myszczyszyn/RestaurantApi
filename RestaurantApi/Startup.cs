1dbfc024d00bb30068939b86c6ed9899b5f54474
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-07-20 19:25:17 +0200

    Enable new validation for RestaurantQuery

--- RestaurantApi/Startup.cs
+++ RestaurantApi/Startup.cs
@@ -77,6 +77,7 @@ namespace RestaurantApi
             services.AddScoped<ErrorHandlingMiddleware>();
             services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
             services.AddScoped<IValidator<RegisterUserDto>,RegisterUserDtoValidator>();
+            services.AddScoped<IValidator<RestaurantQuery>,RestaurantQueryValidator>();
             services.AddScoped<RequestTimeMiddleware>();
             services.AddScoped<IUserContextService, UserContextService>();
             services.AddHttpContextAccessor();
