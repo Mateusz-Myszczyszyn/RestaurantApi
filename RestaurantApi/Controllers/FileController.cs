73d599b8e296b39a8b0082837c9704ae6d237c16
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-08-02 19:30:55 +0200

    Create file handling by enabling it for user to download file from web and to send file into server

---
+++ RestaurantApi/Controllers/FileController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApi.Controllers
+{
+    [Route("file")]
+    [ApiController]
+    [Authorize]
+    public class FileController : ControllerBase
+    {
+        [HttpGet]
+        [ResponseCache(Duration =1200,VaryByQueryKeys = new[] {"fileName"})] 
+        public ActionResult GetFile([FromQuery]string fileName)//getting file from server
+        {
+            var rootPath = Directory.GetCurrentDirectory();
+
+            var filePath = $"{rootPath}/PrivateFiles/{fileName}";
+
+            var fileExist = System.IO.File.Exists(filePath);
+
+            if (!fileExist)
+            {
+                return NotFound();
+            }
+
+            var contentProvider = new FileExtensionContentTypeProvider();
+
+            contentProvider.TryGetContentType(filePath, out string contentType);
+
+            var fileContent = System.IO.File.ReadAllBytes(filePath);
+
+            return File(fileContent,contentType,fileName);
+
+        }
+
+        [HttpPost]
+        public ActionResult Upload([FromForm]IFormFile file)// action for user to send files to server to directory privatefiles
+        {
+            if(file !=null && file.Length > 0)
+            {
+                var rootPath = Directory.GetCurrentDirectory();
+                var fileName = file.Name;
+                var fullPath = $"{rootPath}/PrivateFiles/{fileName}";
+                using(var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+    }
+}
